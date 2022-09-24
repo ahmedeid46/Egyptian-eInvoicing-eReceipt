beaba1d1e73ec061492c3f97dcffd01d7d05d21c
ahmedeid46 <ahmed.eid.1@hotmail.com>
2022-09-24 21:23:14 +0200

    Change config

--- config/config.php
+++ config/config.php
@@ -4,5 +4,8 @@
  * You can place your custom package configuration in here.
  */
 return [
-
+    'apiBaseUrl'=>env('API_BASE_URL','https://api.preprod.invoicing.eta.gov.eg'),
+    'idSrvBaseUrl'=>env('ID_SRV_BASE_URL','https://id.preprod.eta.gov.eg'),
+    'clientId'=>env('CLIENT_ID'),
+    'clientSecret'=>env('CLIENT_SECRET'),
 ];
